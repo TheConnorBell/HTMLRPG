104a87899184554d8ea38d81919d48a5a0827362
TheConnorBell <cabell2003@gmail.com>
2024-12-21 16:24:47 +1030

    Refactor teleporter rendering code to properly use renderer class

--- javascript/renderer.js
+++ javascript/renderer.js
@@ -1,5 +1,8 @@
-// Set an offset for rendering tiles to prevent tiles not appearing during walking.
+// Set an offset for rendering different objects to prevent them not appearing during walking.
 const tileRenderOffset = 1;
+const teleporterRenderOffset = 1;
+const decorationRenderOffset = 5;
+const interactorRenderOffset = 2;
 
 export class Renderer {
 
@@ -10,8 +13,8 @@ export class Renderer {
         this.canvas = canvas;
         this.context = context;
         this.tileSize = tileSize;
-        this.screenCellWidthAmount = screenCellWidthAmount;
-        this.screenCellHeightAmount = screenCellHeightAmount;
+        this.canvasWidthToCenter = Math.floor(screenCellWidthAmount / 2);
+        this.canvasHeightToCenter = Math.floor(screenCellHeightAmount / 2);
         
         // Keep pixels sharp.
         this.context.imageSmoothingEnabled = false;
@@ -35,44 +38,54 @@ export class Renderer {
         }
     }
 
+    isObjectWithinCameraView(xPos, yPos, xOffset, yOffset, positiveXOffset, positiveYOffset, renderOffset) {
+        if (xPos < xOffset - renderOffset || xPos > positiveXOffset + renderOffset || yPos < yOffset - renderOffset || yPos > positiveYOffset + renderOffset) {
+            return false;
+        }
+        return true;
+    }
+
     drawFrame(gameMapCells, gameMapTeleporters, gameMapDecorations, gameMapInteractors, player, mapWidth, mapHeight, currentlyDoingTransition) {
 
         // Clear the frame.
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        // Get the coordinates the top left tile on the visible canvas area.
-        const xOffset = player.getX() - Math.floor(this.screenCellWidthAmount / 2);
-        const yOffset = player.getY() - Math.floor(this.screenCellHeightAmount / 2);
-        
-        // Get the coordinates of the bottom right tile on the visible canvas area.
-        const positiveXOffset = player.getX() + Math.floor(this.screenCellWidthAmount / 2);
-        const positiveYOffset = player.getY() + Math.floor(this.screenCellHeightAmount / 2);
+        const playerX = player.getX();
+        const playerY = player.getY();
 
         const playerSubX = player.getSubX();
         const playerSubY = player.getSubY();
+
+        // Get the coordinates the top left tile on the visible canvas area.
+        const xOffset = playerX - this.canvasWidthToCenter;
+        const yOffset = playerY - this.canvasHeightToCenter;
+        
+        // Get the coordinates of the bottom right tile on the visible canvas area.
+        const positiveXOffset = playerX + this.canvasWidthToCenter;
+        const positiveYOffset = playerY + this.canvasHeightToCenter;
 
         for (var i = 0; i < gameMapCells.length; i++) {
 
             const cell = gameMapCells[i];
 
-            // Determine if the tile falls within the cameras bounds.
-            if (cell.x < xOffset - tileRenderOffset || cell.x > positiveXOffset + tileRenderOffset || cell.y < yOffset - tileRenderOffset || cell.y > positiveYOffset + tileRenderOffset) {
-                continue
+            // Exit the cell array early if all future tiles will be outside of the cameras view.
+            if ((cell.x > (positiveXOffset + tileRenderOffset) && cell.y >= (positiveYOffset + tileRenderOffset)) || cell.y > positiveYOffset + tileRenderOffset) {
+                break;
             }
 
-            const cellScreenXPosition = cell.x - xOffset;
-            const cellScreenYPosition = cell.y - yOffset;
+            // Determine if the tile falls within the cameras bounds.
+            if (!this.isObjectWithinCameraView(cell.x, cell.y, xOffset, yOffset, positiveXOffset, positiveYOffset, tileRenderOffset)) {
+                continue;
+            }
 
-            this.drawTile(this.textureMap[cell.texturePath], Math.floor((cellScreenXPosition + playerSubX) * this.tileSize), Math.floor((cellScreenYPosition + playerSubY) * this.tileSize));
+            // Draw the tile.
+            this.drawObject(
+                this.textureMap[cell.texturePath], 
+                (cell.x - xOffset + playerSubX) * this.tileSize,
+                (cell.y - yOffset + playerSubY) * this.tileSize
+            );
         }
 
-        var renderCellOffset = 1;
-        var maximumXRender = (player.getX() + Math.floor(this.screenCellWidthAmount / 2) + renderCellOffset);
-        var minimumXRender = (player.getX() - Math.floor(this.screenCellWidthAmount / 2) - renderCellOffset);
-        var maximumYRender = (player.getY() + Math.floor(this.screenCellHeightAmount / 2) + renderCellOffset);
-        var minimumYRender = (player.getY() - Math.floor(this.screenCellHeightAmount / 2) - renderCellOffset);
-        // Default teleporter fill colour.
-        this.context.fillStyle = "#15d445";
 
         // Draw teleporters onto the visible game map.
         for (var i = 0; i < gameMapTeleporters.length; i++) {
@@ -80,31 +93,32 @@ export class Renderer {
             const currentTeleporter = gameMapTeleporters[i];
 
             // Check the teleporter is visible on the screen.
-            if (currentTeleporter.visible == 0 || currentTeleporter.x > maximumXRender || currentTeleporter.x < minimumXRender || currentTeleporter.y > maximumYRender || currentTeleporter.y < minimumYRender) {
+            if (!this.isObjectWithinCameraView(currentTeleporter.x, currentTeleporter.y, xOffset, yOffset, positiveXOffset, positiveYOffset, teleporterRenderOffset)) {
                 continue;
             }
 
-            // Draw teleporters using the correct/no texture.
-            if ((currentTeleporter.textureType != "" && !currentlyDoingTransition) || (currentTeleporter.teleporterType == "hole" && currentTeleporter.textureType != "")) {
-    
-                this.context.drawImage(this.textureMap[currentTeleporter.texturePath], Math.floor(((currentTeleporter.x - xOffset) + player.getSubX()) * this.tileSize), Math.floor(((currentTeleporter.y - yOffset) + player.getSubY()) * this.tileSize), this.tileSize, this.tileSize);
-            
-            } else if (currentTeleporter.teleporterType == "door" && currentTeleporter.useTextureType != "" && currentlyDoingTransition) {
-                this.context.drawImage(this.textureMap[currentTeleporter.useTexturePath], Math.floor(((currentTeleporter.x - xOffset) + player.getSubX()) * this.tileSize), Math.floor(((currentTeleporter.y - yOffset) + player.getSubY()) * this.tileSize), this.tileSize, this.tileSize);
-            
-            } else {
-                // Draw the teleporter with the default colour.
-                this.context.fillRect(Math.floor(((currentTeleporter.x - xOffset) + player.getSubX()) * this.tileSize), Math.floor(((currentTeleporter.y - yOffset) + player.getSubY()) * this.tileSize), this.tileSize, this.tileSize);
+            var teleporterTexturePath = this.textureMap[currentTeleporter.texturePath];
+
+            // Change the teleporter texture if the player is currently using the teleporter.
+            if (currentTeleporter.teleporterType == "door" && currentTeleporter.useTexturePath != "" && currentlyDoingTransition) {
+                teleporterTexturePath = this.textureMap[currentTeleporter.useTexturePath];
             }
+
+            // Draw the teleporter.
+            this.drawObject(
+                teleporterTexturePath,
+                (currentTeleporter.x - xOffset + playerSubX) * this.tileSize,
+                (currentTeleporter.y - yOffset + playerSubY) * this.tileSize
+            );
         }
 
         var decorationsInFrontOfPlayer = [];
         
-        renderCellOffset = 3;
-        maximumXRender = (player.getX() + Math.floor(this.screenCellWidthAmount / 2) + renderCellOffset);
-        minimumXRender = (player.getX() - Math.floor(this.screenCellWidthAmount / 2) - renderCellOffset);
-        maximumYRender = (player.getY() + Math.floor(this.screenCellHeightAmount / 2) + renderCellOffset);
-        minimumYRender = (player.getY() - Math.floor(this.screenCellHeightAmount / 2) - renderCellOffset);
+        //renderCellOffset = 3;
+        var maximumXRender = 1;
+        var minimumXRender = 10;
+        var maximumYRender = 1;
+        var minimumYRender = 10;
 
         // Go through each decoration, and determine if they need to be rendered before or after the player is drawn.
         for (var i = 0; i < gameMapDecorations.length; i++) {
@@ -181,18 +195,14 @@ export class Renderer {
     }
 
     drawPlayer(player) {
-        this.context.drawImage(this.textureMap[player.getTexturePath()], ((this.tileSize/2) * player.getCurrentWalkPose()), (player.getOrientation() * this.tileSize), this.tileSize/2, this.tileSize, this.tileSize*Math.floor(this.screenCellWidthAmount/2), this.tileSize*Math.floor(this.screenCellHeightAmount/2) - this.tileSize, this.tileSize, this.tileSize*2);
+        this.context.drawImage(this.textureMap[player.getTexturePath()], ((this.tileSize/2) * player.getCurrentWalkPose()), (player.getOrientation() * this.tileSize), this.tileSize/2, this.tileSize, this.tileSize * this.canvasWidthToCenter, this.tileSize * this.canvasHeightToCenter - this.tileSize, this.tileSize, this.tileSize*2);
     }
 
-    drawTile(texturePath, xPos, yPos) {
-        this.context.drawImage(texturePath, xPos, yPos, this.tileSize, this.tileSize);
+    drawObject(texturePath, xPos, yPos, width = 1, height = 1) {
+        this.context.drawImage(texturePath, xPos, yPos, this.tileSize * width, this.tileSize * height);
     }
 
     drawCharacter() {
-
-    }
-
-    drawObject() {
 
     }
 }
